c55474d20a0c8814afefe153f3f3f6cc9c5eed15
Sareh Safuan <sarehsafuan@gmail.com>
2020-06-01 21:45:23 +0800

    add dist folder to gitignore

--- dist/src/validator/create_user.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function default_1(req, res, next) {
-    console.log('yes');
-    next();
-}
-exports.default = default_1;
